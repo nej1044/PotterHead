033d6c3b7480d68c32f3e5b918e618568ed79905
Hailey Roh <nej1044@naver.com>
2021-09-18 21:51:27 +0900

    영화 선택 시 영화 소개 창 열기 (우선 마법사의 돌만 추가했음), 닫기 버튼 클릭 시 영화 소개 창 닫기

--- js/series.js
+++ js/series.js
@@ -1,3 +1,22 @@
+// 메뉴 이름 굵게
 const currentId = document.querySelector('#series');
 
 currentId.style.fontWeight = "bold";
+
+// 영화 선택
+const currentMovie = document.querySelector('#stone-logo');
+const movieContainer = document.querySelector('#movie-container');
+
+movieContainer.style.display = 'none';
+
+currentMovie.addEventListener('click', function() {
+  movieContainer.style.display = 'block';
+  document.getElementById('trailer').setAttribute('src', 'videos/stone.mp4');
+})
+
+// movie container close
+const movieClose = document.querySelector('#movie-close');
+
+movieClose.addEventListener('click', function() {
+  movieContainer.style.display = 'none';
+});
