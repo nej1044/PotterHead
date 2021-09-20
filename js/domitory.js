ab0caeb8a53c07e100eb6ff23d6420d21b233ad9
Hailey Roh <nej1044@naver.com>
2021-09-21 08:23:56 +0900

    domitory logo 변경 -> mouse over/out 이벤트 추가

--- js/domitory.js
+++ js/domitory.js
@@ -1,3 +1,22 @@
 const currentId = document.querySelector('#domitory');
 
 currentId.style.fontWeight = "bold";
+
+// 마우스 올리면 logo 움직임
+let currentDomitory = '';
+
+document.addEventListener('mouseover', function selectDomitory(event) {
+  if (event.target.className === 'domitory-logo') {
+    currentDomitory = document.querySelector(`#` + `${event.target.id}`);
+    currentDomitory.setAttribute('src', 'images/'+`${event.target.id}`+'.gif');
+  }
+});
+
+// 마우스 나가면 logo 멈춤
+document.addEventListener('mouseout', function selectDomitory(event) {
+  if (event.target.className === 'domitory-logo') {
+    currentDomitory = document.querySelector(`#` + `${event.target.id}`);
+    currentDomitory.setAttribute('src', 'images/'+`${event.target.id}`+'.jpg');
+    currentDomitory = '';
+  }
+});
